7d9738a37ae6196c01dbc7688f5a7c9ad623bb92
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-16 15:47:02 +0200

    [add] ApiKey

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
 export default {
   name: 'App',
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    apiKey() {
+      // axios get api key
+      console.log(store.apiKey);
+      axios.get(store.apiKey)
+      .then((res) => {
+        console.log(res.data);
+      })
+      // error
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  },
+  mounted() {
+    this.apiKey();
+  }
 }
 </script>
 
@@ -11,5 +35,5 @@ export default {
 
 
 <style lang="scss" scoped>
-
+@use './scss/main.scss' as *;
 </style>
